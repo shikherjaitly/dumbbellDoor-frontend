5450a03b0c677249f1dee838ac7fc44d37e8cdf2
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2024-02-20 00:59:06 +0530

    updated navbar details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
+import Home from "./components/Home";
+import Trainers from "./components/Trainers";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Dumbbell Door</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/trainers" element={<Trainers />} />
       </Routes>
     </div>
   );
---
+++ src/components/Trainers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trainers = () => {
+  return (
+    <div>
+      <h1 className=" text-4xl">Find Trainers</h1>
+    </div>
+  );
+};
+
+export default Trainers;
