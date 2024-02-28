b0b3d933e65ee6d3900907f7ad41de0ad1af0ed1
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2024-02-28 22:13:34 +0530

    code update

--- src/components/TrainerProfile.jsx
+++ src/components/TrainerProfile.jsx
@@ -10,20 +10,19 @@ const TrainerProfile = () => {
 
   const [trainer, setTrainer] = useState({});
 
-  const fetchTrainerDetails = async () => {
-    try {
-      const trainerData = await axios.get(
-        `http://localhost:8000/api/trainer/fetchTrainerDetails/${trainerID}`
-      );
-      setTrainer(trainerData.data.message);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const fetchTrainerDetails = async () => {
+      try {
+        const trainerData = await axios.get(
+          `http://localhost:8000/api/trainer/fetchTrainerDetails/${trainerID}`
+        );
+        setTrainer(trainerData.data.message);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     fetchTrainerDetails();
-  }, []);
+  }, [trainerID]);
 
   return (
     <div className="w-full relative [background:linear-gradient(108.87deg,_#00101c,_#00101c,_#29000f)] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-col items-center justify-start pt-[3.813rem] pb-[3.875rem] pr-[3.438rem] pl-[1.25rem] box-border gap-[2.688rem_0rem] tracking-[normal] mq750:gap-[2.688rem_0rem] mq1275:pr-[1.688rem] mq1275:box-border">
